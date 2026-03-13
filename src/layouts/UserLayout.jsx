85c4533feb0ed919ec1e0b4f8afcc520f6aa0a3e
Ahmed Amer <amer73090@gmail.com>
2026-03-13 02:23:41 +0200

    update-scroll

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -8,7 +8,7 @@ const UserLayout = ({ children, transparentNav = false }) => (
   <>
     <Navbar transparent={transparentNav} />
     <main className={transparentNav ? '' : 'pt-[76px]'} style={transparentNav ? {} : { minHeight: '100vh' }}>
-      <div className="min-w-0 w-full overflow-x-hidden">{children}</div>
+      <div className="min-w-0 w-full ">{children}</div>
     </main>
     <Footer />
     <ToastContainer position="bottom-right" autoClose={3500} hideProgressBar />
